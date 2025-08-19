669ad6b7dd18e4b25b940912f371bf56f40f93ac
Asdex1240 <jose.morenobrn@uanl.edu.mx>
2025-08-19 16:24:56 -0600

    feat: upload bundles

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
+    <View style={styles.container}>
+      <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 25.6866,
+        longitude: -100.3161,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}  
+      />
     </View>
   )
 }
 
 export default MapScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  }
+})
